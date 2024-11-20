4c3ef9ef00d5b161b95b1dab3b582e324810c54d
victorgiuliano2162 <vitor.giuliano@gmail.com>
2024-11-20 12:57:12 -0300

    event binding

--- learning/curso-angular/src/app/components/event-binding/event-binding.component.ts
+++ learning/curso-angular/src/app/components/event-binding/event-binding.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-event-binding',
   templateUrl: './event-binding.component.html',
-  styleUrls: ['./event-binding.component.css']
+  styleUrls: ['./event-binding.component.css'],
 })
 export class EventBindingComponent {
 
+  valorSalvo: string = '';
+  valorAtual: string = '';
+  isMouseOver: boolean = false;
+
+  salvarValor(event: any) {
+    this.valorSalvo = event.target.value;
+  }
+
+  salvarValorBlur(event: any) {
+    this.valorSalvo = event;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+    console.log(this.isMouseOver)
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    //console.log((<HTMLInputElement>event.target).value)
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+    //console.log(event)
+  }
+
+  pressedEnter() {
+    console.log(this.valorAtual);
+  }
+  clickedButton() {
+    alert('Clicaste-me');
+  }
 }
