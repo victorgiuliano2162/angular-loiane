eb7711f7b5013d8ac0aee4293f7c67b29c915712
victorgiuliano2162 <vitor.giuliano@gmail.com>
2024-11-20 11:15:04 -0300

    improvment email validation

--- learning/curso-angular/src/app/components/forms-validator/forms-validator.component.ts
+++ learning/curso-angular/src/app/components/forms-validator/forms-validator.component.ts
@@ -15,4 +15,12 @@ export class FormsValidatorComponent {
   onSubmit(a) {
     console.log(a)
   }
+
+  doubleCampsValidation(input1: string, input2: string): boolean {
+    if (input2.trim().length === 0) return false
+    else if (input2 !== input1) return true
+    //input2 está em branco, não faz nada
+    //input2 não é branco e diferente do input1, faça algo
+    return false;
+  }
 }
