727c5a7d54c8cb80eb48a4f0a1984d4f8b10e4ea
victorgiuliano2162 <vitor.giuliano@gmail.com>
2024-11-20 12:57:02 -0300

    .

--- learning/curso-angular/src/app/components/forms-validator/forms-validator.component.ts
+++ learning/curso-angular/src/app/components/forms-validator/forms-validator.component.ts
@@ -19,8 +19,8 @@ export class FormsValidatorComponent {
   doubleCampsValidation(input1: string, input2: string): boolean {
     if (input2.trim().length === 0) return false
     else if (input2 !== input1) return true
-    //input2 está em branco, não faz nada
-    //input2 não é branco e diferente do input1, faça algo
+    //when input2 is blank, do nothing
+    //when input2 is not blank and diffrent from input2 return error message
     return false;
   }
 }
